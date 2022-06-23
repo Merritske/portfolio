bb758cdd0182ef0a6d3215d6e4d44ed087cd5431
merritske <merritske@gmail.com>
2022-06-23 20:00:13 +0200

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,19 +10,19 @@ function About() {
   }, {
     name: "CSS",
     level: "90%"
-  },{
+  }, {
     name: "SASS",
     level: "70%"
-  },  {
+  }, {
     name: "Bootstrap",
     level: "80%"
-  },{
+  }, {
     name: "JavaScript",
     level: "90%"
   }, {
     name: "React",
     level: "80%"
-  },{
+  }, {
     name: "Redux",
     level: "50%"
   }, {
@@ -46,7 +46,7 @@ function About() {
   }, {
     name: "Heroku",
     level: "50%"
-  },{
+  }, {
     name: "Vercel",
     level: "80%"
   },]
@@ -56,13 +56,13 @@ function About() {
   }, {
     name: "Engels",
     level: "80%"
-  },{
+  }, {
     name: "Frans",
     level: "60%"
-  },{
+  }, {
     name: "Duits",
     level: "60%"
-  },{
+  }, {
     name: "Spaans",
     level: "20%"
   }]
@@ -72,77 +72,111 @@ function About() {
   }, {
     name: "Illustrator",
     level: "60%"
-  },{
+  }, {
     name: "Freehand",
     level: "70%"
   }]
+  const extra = [
+    {
+      name: "Presenteren",
+      level: "95%"
+    }, {
+      name: "Luistervaardigheid",
+      level: "80%"
+    },
+    {
+      name: "Leervermogen",
+      level: "90%"
+    }
+  ]
 
   return (
     <div className='container'>
       <h1>SKILLZ</h1>
-<h2 style={{"textAlign": "left"}}>Programmeertalen:</h2>
-       {skillz.map((skill, index) => (
+      <h2>Programmeertalen:</h2>
+      {skillz.map((skill, index) => (
         <div className='bar' key={index}>
-          <div className="bar-label" >
+          <div className="bar-label" style={{ "width": "120px" }} >
             {skill.name}
           </div>
           <div className="bar-containLevel">
-              <div className="bar-contain " style={{"width": `${skill.level}`}}>
-         
-          </div>  
-          <div className="bar-level">
-             {skill.level}
+            <div className="bar-contain " style={{ "width": `${skill.level}` }}>
+
+            </div>
+            <div className="bar-level">
+              {skill.level}
             </div>
           </div>
-        
+
         </div>))
-      } 
+      }
+
       <h3>Wat ik nog niet ken, zal ik snel leren.</h3>
-      <h2 style={{"textAlign": "left"}}>Talen:</h2>
+      <hr />
+      <h2 >Talen:</h2>
       {talen.map((skill, index) => (
         <div className='bar' key={index}>
-          <div className="bar-label" >
+          <div className="bar-label" style={{ "width": "120px" }}>
             {skill.name}
           </div>
           <div className="bar-containLevel">
-              <div className="bar-contain " style={{"width": `${skill.level}`}}>
-         
-          </div>  
-          <div className="bar-level">
-             {skill.level}
+            <div className="bar-contain " style={{ "width": `${skill.level}` }}>
+
+            </div>
+            <div className="bar-level">
+              {skill.level}
             </div>
           </div>
-        
+
         </div>))
-      } 
-      <h2 style={{"textAlign": "left"}}>Computerprogramma's:</h2>
-  {computerprogramma.map((skill, index) => (
+      }
+      <hr />
+      <h2 >Computerprogramma's:</h2>
+      {computerprogramma.map((skill, index) => (
         <div className='bar' key={index}>
-          <div className="bar-label" >
+          <div className="bar-label" style={{ "width": "110px" }}>
             {skill.name}
           </div>
           <div className="bar-containLevel">
-              <div className="bar-contain " style={{"width": `${skill.level}`}}>
-         
-          </div>  
-          <div className="bar-level">
-             {skill.level}
+            <div className="bar-contain " style={{ "width": `${skill.level}` }}>
+
+            </div>
+            <div className="bar-level">
+              {skill.level}
             </div>
           </div>
-        
+
         </div>))
-      } 
-<div className="container">
+      }
+      <hr />
+      <h2 >Extra's:</h2>
+      {extra.map((skill, index) => (
+        <div className='bar' key={index}>
+          <div className="bar-label" style={{ "width": "180px" }}>
+            {skill.name}
+          </div>
+          <div className="bar-containLevel" style={{ "width": "70%" }}>
+            <div className="bar-contain " style={{ "width": `${skill.level}` }}>
+
+            </div>
+            <div className="bar-level">
+              {skill.level}
+            </div>
+          </div>
+
+        </div>))
+      }
+      <div className="container">
         <h1 >REFERENCES</h1>
-      <div className='references'>
+        <div className='references'>
           <h3>Atilla Balin: instructeur frontend development </h3>
           <a href='mailto:atilla.balin@intecbrussel.be' >atilla.balin@intecbrussel.be</a>
-      </div>
-      <div className='references'>
+        </div>
+        <div className='references'>
           <h3>Ruud Marks: teamleider tewerkstelling en sociale vaardigheden </h3>
           <a href='mailto:ruud.marks@intecbrussel.be' >ruud.marks@intecbrussel.be</a>
+        </div>
       </div>
-</div>
 
     </div>
   )
